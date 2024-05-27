60ce9b58d990436c58adf8fa3cac73e6e94b595c
Maksim <mksmvnv@icloud.com>
2024-05-27 15:40:00 +0300

    Write a handler for the start command

---
+++ core/handlers/start.py
@@ -0,0 +1,11 @@
+from aiogram import Router
+from aiogram.types import Message
+from aiogram.filters import CommandStart
+
+
+start_router = Router()
+
+
+@start_router.message(CommandStart())
+async def command_start(message: Message) -> None:
+    await message.answer("Авторизация прошла успешно! Какой у вас вопрос?")
