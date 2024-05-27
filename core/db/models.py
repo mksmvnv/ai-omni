8bd28e37dae8387384408537b13bd23e515e1cc6
Maksim <mksmvnv@icloud.com>
2024-05-27 16:36:58 +0300

    Add a Calculate model for reports on the division of payment for the cost of the service

---
+++ core/db/models.py
@@ -0,0 +1,14 @@
+from sqlalchemy import Integer, BigInteger, String
+from sqlalchemy.orm import Mapped, mapped_column
+
+from db.engine import BaseModel
+
+
+class Calculate(BaseModel):
+    __tablename__ = "сalculate_tokens"
+
+    id: Mapped[int] = mapped_column(Integer, primary_key=True)
+    user_id: Mapped[int] = mapped_column(BigInteger)
+    user_name: Mapped[str] = mapped_column(String)
+    user_tokens: Mapped[int] = mapped_column(Integer)
+    ai_tokens: Mapped[int] = mapped_column(Integer)
