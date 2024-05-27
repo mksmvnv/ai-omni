c9676f33f20d796c527ea1f65fbcd63498bfef46
Maksim <mksmvnv@icloud.com>
2024-05-27 16:38:08 +0300

    Add character counting from the user and AI for subsequent data entry into the database

--- core/handlers/generate.py
+++ core/handlers/generate.py
@@ -4,6 +4,7 @@ from aiogram.fsm.context import FSMContext
 
 from utils.ai import ai_omni
 from states.user import Generate
+from db.config import total_tokens
 
 
 generate_router = Router()
@@ -18,12 +19,17 @@ async def generate(message: Message, state: FSMContext) -> None:
     data = await state.get_data()
 
     prompt = data.get("prompt", [])
-
     prompt.append({"role": "user", "content": message.text})
 
     completion = await ai_omni(prompt)
-
     prompt.append({"role": "assistant", "content": completion})
+
+    user_tokens = len(message.text)
+    ai_tokens = len(completion)
+
+    await total_tokens(
+        message.from_user.id, message.from_user.username, user_tokens, ai_tokens
+    )
 
     await state.update_data(prompt)
 
