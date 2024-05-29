1b39b99076fd6d863e9677794daacebe3bc8f210
Maksim <mksmvnv@icloud.com>
2024-05-29 20:05:17 +0300

    Add data update to state when start command is pressed

--- core/handlers/start.py
+++ core/handlers/start.py
@@ -1,11 +1,13 @@
 from aiogram import Router
 from aiogram.types import Message
 from aiogram.filters import CommandStart
+from aiogram.fsm.context import FSMContext
 
 
 start_router = Router()
 
 
 @start_router.message(CommandStart())
-async def command_start(message: Message) -> None:
+async def command_start(message: Message, state: FSMContext) -> None:
     await message.answer("Авторизация прошла успешно! Какой у вас вопрос?")
+    await state.update_data(prompt = [])
