f1c0e70a152d0eeff924e0946f8df874ec7bdbc5
Maksim <mksmvnv@icloud.com>
2024-08-25 20:04:18 +0300

    Rename env vars

--- core/utils/config.py
+++ core/utils/config.py
@@ -2,11 +2,14 @@ from pydantic_settings import BaseSettings
 
 
 class Settings(BaseSettings):
-    tg_token: str
-    tg_ids: str
-    ai_token: str
-    ai_base_url: str
-    postgres_url: str
+    TG_TOKEN: str
+    TG_IDS: str
+    AI_TOKEN: str
+    AI_BASE_URL: str
+    DATABASE_URL: str
+    POSTGRES_USER: str
+    POSTGRES_PASSWORD: str
+    POSTGRES_DB: str
 
     class Config:
         env_file = ".env"
