4e6224194bf8a9051d3c029216bf0b5e9022ab1e
Maksim <mksmvnv@icloud.com>
2024-08-26 20:03:00 +0300

    Add set new state after update data

--- core/handlers/start.py
+++ core/handlers/start.py
@@ -3,6 +3,8 @@ from aiogram.types import Message
 from aiogram.filters import CommandStart
 from aiogram.fsm.context import FSMContext
 
+from states.user import Generate
+
 
 start_router = Router()
 
@@ -11,3 +13,4 @@ start_router = Router()
 async def command_start(message: Message, state: FSMContext) -> None:
     await message.answer("Авторизация прошла успешно! Какой у вас вопрос?")
     await state.update_data(prompt=[])
+    await state.set_state(Generate.text)
